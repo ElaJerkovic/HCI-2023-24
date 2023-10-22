28ea771f7a0ea9bbadf5e9504398dbcee8e57b64
ElaJerkovic <ela.jerkovic1712@gmail.com>
2023-10-22 22:42:35 +0200

    nav bar

---
+++ semester-project/app/gifts/page.tsx
@@ -0,0 +1,5 @@
+export default function Gifts() {
+  return (
+    <h1 className="flex justify-center p-14 text-4xl">Gift Ideas</h1>
+  )
+}
---
+++ semester-project/app/shop/page.tsx
@@ -0,0 +1,5 @@
+export default function Shop() {
+  return (
+    <h1 className="flex justify-center p-14 text-4xl">Shop</h1>
+  )
+}
