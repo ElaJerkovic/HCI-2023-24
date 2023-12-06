6cda27bb026106cc8fd6e294205954e6461ac945
ElaJerkovic <ela.jerkovic1712@gmail.com>
2023-12-06 22:18:20 +0100

    smaller hero section

--- semester-project/app/_components/HeroSection.tsx
+++ semester-project/app/_components/HeroSection.tsx
@@ -25,11 +25,11 @@ const HeroSection = () => (
                 objectFit: "cover",
               }}
             />
-            <div className="absolute inset-0  bg-white opacity-75 w-3/4 h-3/4 mx-auto my-auto"></div>
+            <div className="absolute inset-0  bg-white opacity-75 w-2/3 h-3/4 mx-auto my-auto"></div>
             <div className="absolute top-0 left-0 right-0 bottom-0 z-10 flex flex-col justify-center items-center">
                 <div className="flex flex-col justify-center gap-5 max-w-xl z-10">
                     
-                    <h1 className="hidden md:block text-xl self-center text-center font-lato font-semibold text-zinc-800 uppercase tracking-wide">
+                    <h1 className="hidden md:block text-2xl self-center text-center font-lato font-semibold text-zinc-800 uppercase tracking-wide">
                       Elevate Your Style with Handcrafted Polymer Clay Jewelry!
                     </h1>
                     <h1 className="md:hidden text-xl self-center text-center font-lato font-semibold text-zinc-800 uppercase tracking-wide">
@@ -45,8 +45,8 @@ const HeroSection = () => (
                     <Link href="/shop">
                       <Button
                         zinc
-                        className="text-base xl:text-lg xl:px-8"
-                        iconClassName="xl:w-4 xl:h-4"
+                        className="text-base xl:text-md xl:px-8"
+                        iconClassName="xl:w-3 xl:h-3"
                       >
                         Visit shop
                       </Button>
