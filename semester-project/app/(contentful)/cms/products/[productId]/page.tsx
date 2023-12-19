89000b9e8537a7e918f508dd7e647fbafbc44016
ElaJerkovic <ela.jerkovic1712@gmail.com>
2023-12-19 21:52:04 +0100

    cms

--- semester-project/app/(contentful)/cms/products/[productId]/page.tsx
+++ semester-project/app/(contentful)/cms/products/[productId]/page.tsx
@@ -1,6 +1,77 @@
 import Image from "next/image";
 import { Button } from "../../../../components/ui/button";
 import { Badge } from "../../../../components/ui/badge";
+import { HeroImage } from "../page";
+import { products } from "../productList";
+import contentfulService from "@/app/lib/contentfulClient";
+
+interface CurrencySymbolMapping {
+  [code: string]: string;
+}
+
+const currencySymbolMapping: CurrencySymbolMapping = {
+  USD: "$",
+  GBP: "£",
+  EUR: "€",
+};
+
+type Params = {
+  productId: string;
+};
+
+const ProductPage = async ({ params }: { params: Params }) => {
+  const product = await contentfulService.getProductById(params.productId);
+
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
+  return (
+    <main className="container flex flex-col items-center gap-10 mb-10">
+      <h1 className="font-roboto-condensed text-6xl font-extrabold text-brand-purple-900 my-4">
+        {product?.name}
+      </h1>
+      <div className="grid grid-cols-2 gap-4 w-3/4 relative">
+        <Badge className="absolute top-4 left-4 z-40" variant="entertainment">
+          {product.currencyCode && currencySymbolMapping[product.currencyCode]}
+          {product.price}
+        </Badge>
+        <HeroImage
+          image={product?.heroImage}
+          productName={product.name}
+          className="w-full h-[400px]"
+        />
+        <div className="flex flex-col gap-4 justify-between">
+          <div className="grid grid-cols-2 gap-2">
+            {product.images?.map((image) => (
+              <div key={image} className="relative w-full h-32">
+                <Image
+                  fill
+                  style={{ objectFit: "cover" }}
+                  className="rounded-md"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  src={image as string}
+                  alt={product.name as string}
+                />
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col">
+            <div className="mb-2">{product.description}</div>
+            <Button variant="emph">+ Add to cart </Button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default ProductPage;
+
+
+/*import Image from "next/image";
+import { Button } from "../../../../components/ui/button";
+import { Badge } from "../../../../components/ui/badge";
 
 import {
   CommonNode,
@@ -12,7 +83,7 @@ import hljs from "highlight.js/lib/core";
 import javascript from "highlight.js/lib/languages/javascript";
 import python from "highlight.js/lib/languages/python";
 import "highlight.js/styles/github-dark.css";
-import { RichTextLinksFragment } from "@/marketing-web/app/gql/graphql";
+
 import { HeroImage } from "../page";
 import { products } from "../productList";
 import contentfulService from "@/app/lib/contentfulClient";
@@ -115,8 +186,8 @@ const ProductPage = async ({ params }: { params: Params }) => {
         dangerouslySetInnerHTML={{
           __html: documentToHtmlString(product?.richTextDescription?.json),
         }}
-      /> */}
-      <div className="mt-10 prose prose-h1:text-brand-purple-800">
+      /> */
+     /* <div className="mt-10 prose prose-h1:text-brand-purple-800">
         {documentToReactComponents(
           product.richTextDescription?.json,
           renderOptions(product.richTextDescription?.links)
@@ -126,5 +197,5 @@ const ProductPage = async ({ params }: { params: Params }) => {
   );
 };
 
-export default ProductPage;
+export default ProductPage;*/
 
