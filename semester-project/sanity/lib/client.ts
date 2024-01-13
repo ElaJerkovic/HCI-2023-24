a30e078447757c20f2ebd116af099efd7b924458
ElaJerkovic <ela.jerkovic1712@gmail.com>
2024-01-13 18:34:52 +0100

    sanity

--- semester-project/sanity/lib/client.ts
+++ semester-project/sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token: "sksKFbonF7Jtgk80Ca6u9Hl9FjqKdrp7X9F2cGVBmC7hZPFtkBA0KOGW2e6Y2hFLXSa1s3ESmvPHAWak4clXEn4FDgPplJMI8yXbRfUBvGEeLrN9QA9E0Xb7NGZyeuKmFo9CRklBNpKPCqmGVojs1VfKdpTjYIF6VpYSS3qJr8qr7i7Y8yGw"
+  //token: "sksKFbonF7Jtgk80Ca6u9Hl9FjqKdrp7X9F2cGVBmC7hZPFtkBA0KOGW2e6Y2hFLXSa1s3ESmvPHAWak4clXEn4FDgPplJMI8yXbRfUBvGEeLrN9QA9E0Xb7NGZyeuKmFo9CRklBNpKPCqmGVojs1VfKdpTjYIF6VpYSS3qJr8qr7i7Y8yGw"
 })
