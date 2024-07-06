7e39e9a62519fb39c9ac1727e13b66a2c48268b6
ElaJerkovic <92401418+ElaJerkovic@users.noreply.github.com>
2024-07-07 01:36:31 +0200

    Delete semester-project/app directory

--- semester-project/app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-import Link from "next/link";
-import React from "react";
-
-export default function About() {
-  return (
-    <h1 className="flex justify-center p-14 text-4xl">About us</h1>
-  )
-}
