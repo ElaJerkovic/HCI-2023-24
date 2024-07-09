5bac498b708d7e47398f2959c0a7c1b2493ebcc5
ElaJerkovic <ela.jerkovic1712@gmail.com>
2024-07-09 23:40:54 +0200

    about us images

--- semester-project/app/about/page.tsx
+++ semester-project/app/about/page.tsx
@@ -22,7 +22,7 @@ export default async function About() {
           </p>
         </div>
         
-        <div className="mb-12 mt-16 flex w-full md:mb-16 lg:w-2/3">
+        <div className="mb-12 mt-16 px-4 flex w-full md:mb-16 lg:w-2/3 sm:py-4">
           <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
             {data && data.image1 && ( 
               <Image
